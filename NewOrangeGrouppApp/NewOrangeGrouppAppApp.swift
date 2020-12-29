455b736d85d7c970e99f15d7875017e3367e5519
lina <linasigridli@gmail.com>
2020-12-29 12:01:00 +0100

    Initial Commit

---
+++ NewOrangeGrouppApp/NewOrangeGrouppAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewOrangeGrouppAppApp.swift
+//  NewOrangeGrouppApp
+//
+//  Created by Lina Li on 2020-12-29.
+//
+
+import SwiftUI
+
+@main
+struct NewOrangeGrouppAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
